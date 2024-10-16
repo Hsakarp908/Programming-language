74272b47fa37d3e15135b13f8a1856d899cc8ea6
Hsakarp908 <omparida90400@gmail.com>
2024-10-16 21:25:05 +0530

    add commit

---
+++ Java/CONDITION/calculatesum.java
@@ -0,0 +1,17 @@
+package CONDITION;
+import java.util.*;
+public class calculatesum {
+	public static int calculateSum(int a, int b) {
+		int product=a*b;
+		System.out.print("product is :" + product);
+		return product;
+	}
+	public static void main(String args [])
+	{
+		Scanner sc= new Scanner(System.in);
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		calculateSum(a,b);
+		sc.close();
+	}
+}
