100efdbbafee858cf916cbcf76a4303ebec82478
Hsakarp908 <omparida90400@gmail.com>
2024-11-11 22:18:45 +0530

    NOTHING CHANG

--- Java/OBJECT/inheritance.java
+++ Java/OBJECT/inheritance.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package OBJECT;
 
 public class inheritance {
@@ -20,26 +20,3 @@ public class inheritance {
 		class Cat extends Dog1{  
 			void meow(){System.out.println("meowing...");}  
 			}
-=======
-package OBJECT;
-
-public class inheritance {
-	public static void main(String[] args)
-	    {
-		Cat c=new Cat();  
-		c.meow();  
-		c.bark();
-		c.eat();  
-	
-		}
-	}class Animal1{  
-			void eat()
-			{System.out.println("eating...");}  
-			}  
-		class Dog1 extends Animal1{  
-			void bark(){System.out.println("barking...");}  
-			}  
-		class Cat extends Dog1{  
-			void meow(){System.out.println("meowing...");}  
-			}
->>>>>>> da6d09bca645154be82f7be50740ab9a7b25b477
