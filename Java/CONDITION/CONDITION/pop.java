8b11544cbd73e363455c0f3aedcbf75b2277686d
Hsakarp908 <omparida90400@gmail.com>
2024-11-22 00:48:59 +0530

    update

---
+++ Java/CONDITION/CONDITION/pop.java
@@ -0,0 +1,10 @@
+package CONDITION;
+import java.util.Scanner;
+
+public class pop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("string " + s);
+    }
+}
---
+++ Java/CONDITION/CONDITION/tempCodeRunnerFile.java
@@ -0,0 +1,20 @@
+package CONDITION;
+
+import java.util.Scanner;
+
+public class tempCodeRunnerFile {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        int i = sc.nextInt();
+        double d =sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+        
+
+        System.out.println("string " + s);
+       System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
